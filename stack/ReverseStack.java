29470dcf95724a3027abd83f19bb517c12e23bd4
Unnimaya T <Unnimaya.T@godigit.com>
2024-08-01 10:19:02 +0530

    reverse-stack

--- stack/ReverseStack.java
+++ stack/ReverseStack.java
@@ -1,39 +1,27 @@
 package stack;
 import java.util.*;
 public class ReverseStack {
-    static Stack<Character> st = new Stack<>();
-
-    static void insert_at_bottom(char x)
-    {
-        if (st.isEmpty())
-            st.push(x);
-        else {
-            char a = st.peek();
-            st.pop();
-            insert_at_bottom(x);
-            st.push(a);
-        }
-    }
+    static Stack<Integer> stack1 = new Stack<>();
+    static Stack<Integer> stack2 = new Stack<>();
     static void reverse()
     {
-        if (st.size() > 0) {
-            char x = st.peek();
-            st.pop();
-            reverse();
-            insert_at_bottom(x);
+        while (!stack1.isEmpty()){
+            int data=stack1.peek();
+            stack2.push(data);
+            stack1.pop();
         }
     }
     public static void main(String[] args)
     {
-        st.push('1');
-        st.push('2');
-        st.push('3');
-        st.push('4');
+        stack1.push(1);
+        stack1.push(2);
+        stack1.push(3);
+        stack1.push(4);
 
         System.out.println("Original Stack");
-        System.out.println(st);
+        System.out.println(stack1);
         reverse();
         System.out.println("Reversed Stack");
-        System.out.println(st);
+        System.out.println(stack2);
     }
 }
